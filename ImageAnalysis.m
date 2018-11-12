3dd7a25ec86b307569be63ca75e36c1275e023dc
Onurcan Bekta <onurcan.bektas@metu.edu.tr>
2018-11-12 19:20:05 +0200

    update

--- ImageAnalysis.m
+++ ImageAnalysis.m
@@ -12,13 +12,20 @@ addpath(output_dir)
 
 
 i=1;
+imageFileName = strcat('c', int2str(i), '0.jpg'); %X
+outputFileName = '29072017X-test.txt'; %X
 pixelToMMRatioInX = 10 ./ 86;
+pixelToMMRatioInY = 10 ./ 140; % 29.07
 
 
 RGP = imread([scan_dir, imageFileName]);
 image = rgb2gray(RGP);
 
 
+isItXaxis = false; %X
+imageInspection_ON = false; %X
+dataInspection_ON = false; %X
+makeFit_ON =  true;%X
 saveFitResults = false;
 
 
@@ -27,6 +34,7 @@ if imageInspection_ON == true
 	imshow(image)
 	[x,y] = ginput(3)
 end
+
 
 if isItXaxis == false
 	data = sum(image, 1);
@@ -41,6 +49,7 @@ end
 
 
 if (dataInspection_ON == true)
+	xAxis = 1:1:sizeOfData; 
 	figure(2)
 	plot(xAxis, data, 'x')
 	[x,y] = ginput(2)
@@ -57,6 +66,7 @@ firstCutEnd = 308;
 secondCutBeg = 421;
 secondCutEnd = 557;
 
+
 index = 1:1:sizeOfData;
 switch numberOfCutsToMake
 	case 0
@@ -72,8 +82,10 @@ switch numberOfCutsToMake
 		"Dude, I cannot make that much cut!"
 end
 
+
 cleanIndex = double(cleanIndex)';
 cleanData = double(cleanData)';
+
 
 if makeFit_ON == true
 	t = [max(cleanData), mean(cleanIndex), 20, min(cleanData)];
@@ -81,6 +93,7 @@ if makeFit_ON == true
 	model = @(x) x(1) .* exp( -(  (cleanIndex - x(2)).^2  )./(2 .* (x(3).^2))   )  + x(4) - cleanData;
 	[soln ,ssq,cnt, res, XY] = LMFnlsq(model,t,'Display',1,'XTol', 1e-7,'Lambda', 1);
 
+	
 	figure(3)
 	plot(cleanIndex, cleanData, 'x')
 	hold on
@@ -100,6 +113,8 @@ if makeFit_ON == true
 	    save_dir = strcat(output_dir, outputFileName);
 		pFile = fopen (save_dir, 'a+');
 		magneticFields = [0.01567, 0.03088, 0.04655, 0.06222, 0.07743, 0.09310, 0.10832, 0.12398, 0.13965, 0.15531, 0.17053, 0.18620, 0.20142, 0.21708, 0.23230, 0.24797, 0.26363, 0.27885, 0.29452, 0.31018, 0.32540, 0.34107];
+		magneticFieldStr = magneticFields(i); %X
+		fprintf(pFile, '\n%s %f %.5f %d %d %d %d %d %d %d', imageFileName, sigma, magneticFieldStr, numberOfCutsToMake, dataIndexBeg, dataIndexEnd, firstCutBeg, firstCutEnd, secondCutBeg, secondCutEnd);
 	end
 
 end
