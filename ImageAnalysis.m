4a2c916e809edd60b68cdc40b0340194f41ede71
Onurcan Bekta <onurcan.bektas@metu.edu.tr>
2018-11-12 19:15:36 +0200

    Added the corrected files

--- ImageAnalysis.m
+++ ImageAnalysis.m
@@ -1,131 +1,107 @@
 %% @author: Onurcan Bektaş
 %%          onurcan.bektas@metu.edu.tr
 clear
+clear all
+
 lib_dir = strcat(pwd, '/matlabFunctions');
-scan_dir = strcat(pwd, '/scan');
-output_dir = strcat(pwd, '/output/');
+scan_dir = strcat(pwd, '/scan.29072017/');
+output_dir = strcat(pwd, '/output.29072017/');
 addpath(scan_dir)
 addpath(lib_dir)
 addpath(output_dir)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%% STARTING LINE OF EDIT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-filename = strcat('c150.jpg');
-image_inspection_ON = 0;
-data_inspection_ON = 0;
-fit_ON = 1;
 
-% Do we want to save the result of the fitting parameters to the end of file ?
-save_the_fit_parameters = 0;
-result_filename = '0308Y.txt';
-magnetic_field_magnitude = 0.24796652;
 
-%For x -> 0; for y-> 1
-x_or_y =  1;
-pixel_to_mm_X = 7.07 ./ 87;
-pixel_to_mm_Y = 10 ./ 138; %% For 29.07 
+i=1;
+pixelToMMRatioInX = 10 ./ 86;
 
-%% Cleaning the erroneous data 
-numberOfExtraCuts = 1;
-z1 = 1;
-z2 = 322;
-% First extra little cut interval
-fit_extra11 = 206;
-fit_extra12 = 229;
-% Second extra little cut interval
-fit_extra21 = 299;
-fit_extra22 = 342;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%% END OF LINE OF EDIT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-RGP = imread(filename);
+RGP = imread([scan_dir, imageFileName]);
 image = rgb2gray(RGP);
-figure
-imshow(image)
 
-data_sum = sum(image, 2);
-if(image_inspection_ON == 1)
+
+saveFitResults = false;
+
+
+if imageInspection_ON == true
+	figure(1)
+	imshow(image)
+	[x,y] = ginput(3)
+end
+
+if isItXaxis == false
+	data = sum(image, 1);
+	sizeOfData = length(data);
+	pxToMMRatio = pixelToMMRatioInX;
+else
+	data = sum(image, 2);
+	data = data';
+	sizeOfData = length(data);
+	pxToMMRatio = pixelToMMRatioInY;
+end
+
+
+if (dataInspection_ON == true)
+	figure(2)
+	plot(xAxis, data, 'x')
 	[x,y] = ginput(2)
 end
 
-i = 1:1:length(data_sum);
-plot(i, data_sum, 'x')
-if(data_inspection_ON == 1)
-    [x,y] = ginput(4)
+
+numberOfCutsToMake = 0;
+dataIndexBeg = 1;
+dataIndexEnd = sizeOfData;
+
+firstCutBeg = 250;
+firstCutEnd = 308;
+
+secondCutBeg = 421;
+secondCutEnd = 557;
+
+index = 1:1:sizeOfData;
+switch numberOfCutsToMake
+	case 0
+		cleanIndex = dataIndexBeg:1:dataIndexEnd;
+		cleanData = data(dataIndexBeg:dataIndexEnd);
+	case 1
+		cleanIndex = [dataIndexBeg:1:firstCutBeg, firstCutEnd:1:dataIndexEnd];
+		cleanData = [data(dataIndexBeg: firstCutBeg), data(firstCutEnd:dataIndexEnd)];
+	case 2
+		cleanIndex = [dataIndexBeg:1:firstCutBeg, firstCutEnd:1:secondCutBeg, secondCutEnd:1:dataIndexEnd];
+		cleanData = [data(dataIndexBeg:firstCutBeg), data(firstCutEnd:secondCutBeg), data(secondCutEnd:dataIndexEnd)];
+	otherwise
+		"Dude, I cannot make that much cut!"
 end
 
-data = data_sum';
-index = 1:1:length(data_sum);
+cleanIndex = double(cleanIndex)';
+cleanData = double(cleanData)';
 
-%% Extracting the data that we want to use from the given cut intervals
-if(numberOfExtraCuts == 0)
-    wholerange = index(z1: z2)';
-	wholedata = data(z1:z2)';
-elseif (numberOfExtraCuts == 1)
-		wholerange = [index(z1: fit_extra11), index(fit_extra12: z2)]';
-		wholedata = [data(z1:fit_extra11), data(fit_extra12: z2)]';	
-elseif (numberOfExtraCuts == 2)
-	wholerange = [index(z1: fit_extra11), index(fit_extra12: fit_extra21), index(fit_extra22: z2)]';
-	wholedata = [data(z1:fit_extra11), data(fit_extra12: fit_extra21), data(fit_extra22: z2)]';
+if makeFit_ON == true
+	t = [max(cleanData), mean(cleanIndex), 20, min(cleanData)];
+	t = t(:);
+	model = @(x) x(1) .* exp( -(  (cleanIndex - x(2)).^2  )./(2 .* (x(3).^2))   )  + x(4) - cleanData;
+	[soln ,ssq,cnt, res, XY] = LMFnlsq(model,t,'Display',1,'XTol', 1e-7,'Lambda', 1);
+
+	figure(3)
+	plot(cleanIndex, cleanData, 'x')
+	hold on
+	plot(index, soln(1).*exp( -(  (index - soln(2)).^2  )./(2 .* soln(3).^2)  ) + soln(4))
+	legend('Veri', 'fit')
+	xlabel('Görüntü boyutu (pixel)')
+	ylabel('Parlaklık değeri (birimsiz)')
+	title('Parlaklık vs görüntü boyutu grafiği; B = 2.32 kG')
+	hold off
+
+	A = soln(1)
+	mean = soln(2) * pxToMMRatio
+	sigma = soln(3) * pxToMMRatio
+	offset = soln(4)
+
+	if(saveFitResults == true)
+	    save_dir = strcat(output_dir, outputFileName);
+		pFile = fopen (save_dir, 'a+');
+		magneticFields = [0.01567, 0.03088, 0.04655, 0.06222, 0.07743, 0.09310, 0.10832, 0.12398, 0.13965, 0.15531, 0.17053, 0.18620, 0.20142, 0.21708, 0.23230, 0.24797, 0.26363, 0.27885, 0.29452, 0.31018, 0.32540, 0.34107];
+	end
+
 end
-
-if(x_or_y == 0)
-    pixel_to_mm = pixel_to_mm_X;
-elseif(x_or_y == 1)
-    pixel_to_mm = pixel_to_mm_Y;
-end
-wholedata = double(wholedata);
-wholerange = double(wholerange);
-
-
-
-%% Fitting...
-%% x = [A , mean, sigma, offset], where
-%% our model is A .* exp( -(  (t - mean.^2  )./(2 .* sigma.^2)   )  + offset;
-if(fit_ON == 1)
-t = [max(wholedata), mean(wholerange), 20, min(wholedata)];
-t = t(:);
-model = @(x) x(1) .* exp( -(  (wholerange - x(2)).^2  )./(2 .* (x(3).^2))   )  + x(4) - wholedata;
-[soln ,ssq,cnt, res, XY] = LMFnlsq(model,t,'Display',1,'XTol', 1e-7,'Lambda', 1);
-
-
-%% Plotting the fit against data
-r = z1:1:z2;
-figure(3)
-plot(wholerange, wholedata, 'x')
-hold on
-plot(r, soln(1).*exp( -(  (r - soln(2)).^2  )./(2 .* soln(3).^2)  ) + soln(4))
-hold off
-
-A = soln(1)
-mean = soln(2) * pixel_to_mm
-sigma = soln(3) * pixel_to_mm
-offset = soln(4)
-
-if(save_the_fit_parameters == 1)
-    save_dir = strcat(output_dir, result_filename);
-	pFile = fopen (save_dir, 'a+');
-	fprintf(pFile, '\n%s %f %.5f %d %d %d %d %d %d %d',filename, sigma, magnetic_field_magnitude, numberOfExtraCuts, z1, z2, fit_extra11, fit_extra12, fit_extra21, fit_extra22)
-end
-end
-
-
-
-
-
-
-
-
-
 
 
--- quadScanFitThin.m
+++ quadScanFitThin.m
@@ -1,24 +1,20 @@
 %% @author: Onurcan Bektaş
 %%          onurcan.bektas@metu.edu.tr
 clear
+
 lib_dir = strcat(pwd, '/matlabFunctions');
-output_dir = strcat(pwd, '/output');
+output_dir = strcat(pwd, '/output.29072017');
 addpath(lib_dir)
 addpath(output_dir)
-filename = '03082017XORJ.txt';
+
+filename = '29072017Y.txt';
 %% (+1) for defocusing plane, (-1) for focusing plane
 sign_focus_or_defocus = -1;
+
 beam_check_ON = 1;
 %% Needed only if sign_focus_or_defocus is equal to 1
-rowofTheParameterToCheck = 15;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%% END OF LINE OF EDIT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rowofTheParameterToCheck = 6;
+
 fileID = fopen(filename);
 C = textscan(fileID,'%s %f %f %d %d %d %d %d %d %d');
 data = C(1,2);
@@ -28,27 +24,30 @@ data = data.^2;
 B = C(3);
 B = cell2mat(B);
 k = B ./ (0.043835);
-l = 0.244;
+l = 0.244; %radius 0,055m
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% (+1) for defocusing plane, (-1) for focusing plane
 b = (sign_focus_or_defocus) .* k .* l;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-d = 1.88;
+d = 1.6;
 
 if b(1) > 0
-    w = b(1):0.001:(b(length(b)-1) + 0.2);
+    w = b(1):0.001:(b(length(b)-1) + 0.05);
 elseif b(1) < 0
-    w = (b(length(b)-1) - 0.2):0.001:b(1);
+    w = (b(length(b)-1) - 0.05):0.001:b(1);
 end
+
 p = polyfit(b,data,2)
 xfit = polyval(p,w);  
 plot(w,xfit,'r');
 hold on
 plot(b, data, '*')
 title('The measurement of beam size versus 1/f');
-legend('fit','data','Location', 'northwest');
-xlabel(' -1/f  (in 1/m)');
+legend('fit','data','Location', 'north');
+xlabel(' \pm 1/f  (in 1/m) ');
 ylabel('Beam size sigma^2 (in m^2)');
+%text(-1.6, 7.5*10^(-5), 'y = 10^{-4} * (0.2835*x^2 + 0.4427*x + 0.6641)')
 hold off
 
 sigma11 = sqrt(p(1) ./ (d .^2))
@@ -57,17 +56,17 @@ sigma22 = sqrt(( p(3) - (sigma11.^2) - (2.* d .* sigma12)) ./ (d.^2))
 
 emittance1 = sqrt(sigma11.^2 .* sigma22.^2 - sigma12.^2)
 
-kor = acos(emittance1 ./ (sigma11 .* sigma22))
-sin(kor)
+phi = acos(emittance1 ./ (sigma11 .* sigma22));
+kor = sin(phi)
 
 syms thin(kvar)
-thin(kvar) = [1 + kvar * l * d, d; kvar*l, 1]
+thin(kvar) = [1 + kvar * l * d, d; kvar*l, 1];
 temp_thin = matlabFunction(thin);
 thin = @(kvar) temp_thin(kvar);
 
-k_temp = (sign_focus_or_defocus) .* k(rowofTheParameterToCheck)
-thin(k_temp)
-result_thin = thin(k_temp) * [sigma11.^2, sigma12; sigma12, sigma22.^2] * (thin(k_temp)')
+k_temp = (sign_focus_or_defocus) .* k(rowofTheParameterToCheck);
+thin(k_temp);
+result_thin = thin(k_temp) * [sigma11.^2, sigma12; sigma12, sigma22.^2] * (thin(k_temp)');
 
 Endsigma11 = result_thin(1,1).^(1./2)
 Endsigma12 = result_thin(1,2)
